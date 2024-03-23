dc44d5842802ce8d36e8d2dee0c8ca40e26370d3
techcodevelop <techcodevelop@gmail.com>
2024-03-23 16:29:57 -0500

    prueba de conexion

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -2,7 +2,7 @@
 import { pool } from "../db.js";
 
 export const ping = async (req, res) => {
-    const [result] = await pool.query("select 1+1 as result");
+    const [result] = await pool.query("select 1+3 as result");
     //res.json('PONG');
     res.json(result[0]);
   }
